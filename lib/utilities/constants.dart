0ccde16a54835d923f1339ae5f321f1433322062
Sujay Biswas <sujay00005@gmail.com>
2021-05-17 09:15:10 +0530

    pushing code

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+const kTempTextStyle = TextStyle(
+  fontSize: 100.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontSize: 60.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 100.0,
+);
+
+const kTextFieldInputDecoration = InputDecoration(
+    filled: true,
+    fillColor: Colors.white,
+    hintText: "Enter city name",
+    hintStyle: TextStyle(
+      color: Colors.grey,
+    ),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.all(
+        Radius.circular(10.0),
+      ),
+      borderSide: BorderSide.none,
+    ),
+    icon: Icon(
+      Icons.location_city,
+      color: Colors.white,
+    )
+);
